79632a827fb7ba0ec9a50c9f9a1f5c05583221a5
Patricia-Sigei <pattychela97@gmail.com>
2026-09-14 13:12:50 +0300

    setting up worker and ensures it runs at intervals

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,21 @@
+import { PrismaClient } from "./generated/prisma/client.js";
+
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
 console.log("Worker started");
 
+const incidentCount = await prisma.incident.count();
+
+console.log(`There are ${incidentCount} incidents in the database`);
+
 setInterval(() => {
-  console.log("Worker is processing a job...");
+  console.log("Worker is running...");
 }, 5000);
